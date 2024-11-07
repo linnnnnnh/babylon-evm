bc97bcb83903721b8004bb67951b6fd60d7fb893
lin <lin-h@hotmail.fr>
2024-11-07 18:45:31 +0700

    feat: Add burn function

--- src/ByzBTC.sol
+++ src/ByzBTC.sol
@@ -11,4 +11,8 @@ contract ByzBTC is ERC20 {
     function mint(address to, uint256 amount) public {
         _mint(to, amount);
     }
+
+    function burn(address from, uint256 amount) public {
+        _burn(from, amount);
+    }
 }
